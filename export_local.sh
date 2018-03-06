ab696c2c9f23999e9d639f8533153353b84dc4d8
Ondrej Hejda <ondrejh.ck@email.cz>
2018-03-06 21:08:50 +0100

    new apache setting - dont need two locals anymore

--- export_local.sh
+++
@@ -1,3 +0,0 @@
-cp script/plotly*.js /var/www/html/script/
-#cp formular.html /var/www/html/formular.php
-#cp graf.png /var/www/html/
--- import_local.sh
+++
@@ -1,4 +0,0 @@
-cp /var/www/html/index.php index.php
-cp /var/www/html/script . -r
-cp /var/www/html/data . -r
-cp /var/www/html/style . -r
